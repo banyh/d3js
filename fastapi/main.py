c1096fe55ce371a985a850143661ce0b7849ded9
Bany Hung <banyhong@gmail.com>
2022-06-11 00:24:10 +0800

    backup

--- fastapi/main.py
+++ fastapi/main.py
@@ -24,9 +24,106 @@ def read_root():
     return {"Hello": "World"}
 
 
+test_data = [
+    {
+        "error": "WP-2C1D",
+        "problem_id": 42131,
+        "subcode": "8C010005",
+        "symptom_description": "SYSTEM ERROR WP-2C1D",
+        "symptom_id": 4551057,
+        "symptom_title": "WS* COOLING WATER TEMP CONTROL MMDC: WAFER FLOW CONTROL: LCW PUMP ENABLED DRY OR NO FLOW ERROR",
+        "causes": [
+            {
+                "cause_id": 4,
+                "cause_title": "wafer stage PPL leak",
+                "procedures": [
+                    {
+                        "destination": "Service",
+                        "name": "cws754.rep",
+                        "status": "Final",
+                        "subsystem": "WaferStage",
+                        "title": "REMOVE AND INSTALL LCW RETURN TEMP SENSOR IN MK4i PPL",
+                        "type": "Replace",
+                        "parts": [
+                            {
+                                "id": "SERV.646.47404",
+                                "desc": "NXT WS MK4I PPL TEMP SENS ASSY",
+                            },
+                            {
+                                "id": "SERV.502.31876",
+                                "desc": "NXT WS MK4I PCA CONN SEAL SET",
+                            }
+                        ],
+                        "tools": [
+                            {
+                                "id": "SERV.502.17463",
+                                "desc": "CS SR GR4 TOOL TROLLEY",
+                            },
+                            {
+                                "id": "4022.502.81530",
+                                "desc": "NT WS BM3 SERVICE COVER KIT",
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "cause_id": 4551058,
+                "cause_title": "MORE CAUSES POSSIBLE [NXT3 ONLY]",
+                "procedures": [
+                    {
+                        "destination": "Service",
+                        "name": "cws383.oca",
+                        "status": "Final",
+                        "subsystem": "WaferStage",
+                        "title": "NXT3&4 WS LCSW FLOW PROBLEM OR LCW PRESSURE SENSOR WIRING DEFECT",
+                        "type": "Ocap",
+                        "tools": [
+                            {
+                                "id": "SERV.640.55621",
+                                "desc": "TDE CONNECTOR TOOLKIT",
+                            },
+                            {
+                                "id": "4022.502.81530",
+                                "desc": "NT WS BM3 SERVICE COVER KIT",
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "cause_id": 25648264,
+                "cause_title": "HR valve (too far) closed",
+                "procedures": []
+            }
+        ],
+        "issues": [
+            {
+                "factory": "F15B",
+                "jira_id": "LIT1XVBEEA-197109",
+                "event_cd": "WP-2C1D",
+                "subcode": "8C010005",
+                "actions": "",
+                "is_useful": True,
+                "tool_id": "YPAN03",
+            },
+            {
+                "factory": "F15B",
+                "jira_id": "LIT1XVBEEA-190894",
+                "event_cd": "WP-2C1D",
+                "subcode": "8C010005",
+                "actions": "",
+                "is_useful": True,
+                "tool_id": "YPAN03",
+            }
+        ]
+    }
+];
+
+
 @api_app.get("/graph/nodes")
 def get_graph_nodes():
-    return create_graph()
+    return create_graph(test_data)
 
 
 @api_app.get("/graph", response_class=HTMLResponse)
--- fastapi/src/graph.py
+++ fastapi/src/graph.py
@@ -1,101 +1,22 @@
-test_data = [
-    {
-        "error": "WP-2C1D",
-        "problem_id": 42131,
-        "subcode": "8C010005",
-        "symptom_description": "SYSTEM ERROR WP-2C1D",
-        "symptom_id": 4551057,
-        "symptom_title": "WS* COOLING WATER TEMP CONTROL MMDC: WAFER FLOW CONTROL: LCW PUMP ENABLED DRY OR NO FLOW ERROR",
-        "causes": [
-            {
-                "cause_id": 4,
-                "cause_title": "wafer stage PPL leak",
-                "procedures": [
-                    {
-                        "destination": "Service",
-                        "name": "cws754.rep",
-                        "status": "Final",
-                        "subsystem": "WaferStage",
-                        "title": "REMOVE AND INSTALL LCW RETURN TEMP SENSOR IN MK4i PPL",
-                        "type": "Replace",
-                        "parts": [
-                            {
-                                "id": "SERV.646.47404",
-                                "desc": "NXT WS MK4I PPL TEMP SENS ASSY",
-                            },
-                            {
-                                "id": "SERV.502.31876",
-                                "desc": "NXT WS MK4I PCA CONN SEAL SET",
-                            }
-                        ],
-                        "tools": [
-                            {
-                                "id": "SERV.502.17463",
-                                "desc": "CS SR GR4 TOOL TROLLEY",
-                            },
-                            {
-                                "id": "4022.502.81530",
-                                "desc": "NT WS BM3 SERVICE COVER KIT",
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                "cause_id": 4551058,
-                "cause_title": "MORE CAUSES POSSIBLE [NXT3 ONLY]",
-                "procedures": [
-                    {
-                        "destination": "Service",
-                        "name": "cws383.oca",
-                        "status": "Final",
-                        "subsystem": "WaferStage",
-                        "title": "NXT3&4 WS LCSW FLOW PROBLEM OR LCW PRESSURE SENSOR WIRING DEFECT",
-                        "type": "Ocap",
-                        "tools": [
-                            {
-                                "id": "SERV.640.55621",
-                                "desc": "TDE CONNECTOR TOOLKIT",
-                            },
-                            {
-                                "id": "4022.502.81530",
-                                "desc": "NT WS BM3 SERVICE COVER KIT",
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                "cause_id": 25648264,
-                "cause_title": "HR valve (too far) closed",
-                "procedures": []
-            }
-        ],
-        "issues": [
-            {
-                "factory": "F15B",
-                "jira_id": "LIT1XVBEEA-197109",
-                "event_cd": "WP-2C1D",
-                "subcode": "8C010005",
-                "actions": "",
-                "is_useful": True,
-                "tool_id": "YPAN03",
-            },
-            {
-                "factory": "F15B",
-                "jira_id": "LIT1XVBEEA-190894",
-                "event_cd": "WP-2C1D",
-                "subcode": "8C010005",
-                "actions": "",
-                "is_useful": True,
-                "tool_id": "YPAN03",
-            }
-        ]
-    }
-];
+import math
 
 
-def create_graph():
+def calculate_suggested_position(nodes, parent_id):
+    def _count_descendants(n):
+        c = sum([_count_descendants(nodes[child - 1]) for child in n['descendants']])
+        return c + len(n['descendants'])
+
+    parent_node = nodes[parent_id - 1]
+    counts = [_count_descendants(nodes[i - 1]) for i in parent_node['descendants']]
+    total_counts = sum(counts)
+    if total_counts == 0:
+        return
+    for i, c in zip(parent_node['descendants'], counts):
+        nodes[i - 1]['span_angle'] = parent_node['span_angle'] * (c / total_counts)
+        calculate_suggested_position(nodes, i)
+
+
+def create_graph(data):
     nodes = []
     nodes_id = {}
     edges = []
@@ -104,15 +25,24 @@ def create_graph():
     def _create_node(key, display_text, node_type):
         nonlocal last_id
         nodes_id[key] = last_id
-        last_id += 1
         nodes.append({
             "id": nodes_id[key],
             "display_text": display_text,
             "type": node_type,
+            "suggest_x": 0,
+            "suggest_y": 0,
+            "ascendants": [],
+            "descendants": [],
         })
+        last_id += 1
         return nodes_id[key]
 
-    for item in test_data:
+    def _create_edge(source, target):
+        edges.append(dict(source=source, target=target, value=1))
+        nodes[source - 1]['descendants'].append(target)
+        nodes[target - 1]['ascendants'].append(source)
+
+    for item in data:
         event_code_id = nodes_id.get(item['error'], 0)
         if not event_code_id:
             event_code_id = _create_node(item['error'], item['error'], 'event_code')
@@ -120,38 +50,48 @@ def create_graph():
         subcode_id = nodes_id.get(item['subcode'], 0)
         if not subcode_id:
             subcode_id = _create_node(item['subcode'], item['subcode'], 'subcode')
-            edges.append(dict(source=event_code_id, target=subcode_id, value=1))
+            _create_edge(event_code_id, subcode_id)
 
         symptom_id = nodes_id.get(item['symptom_id'], 0)
         if not symptom_id:
             text = item['symptom_description'] + '\n' + item['symptom_title']
             symptom_id = _create_node(item['symptom_id'], text, 'symptom')
-            edges.append(dict(source=subcode_id, target=symptom_id, value=1))
+            _create_edge(subcode_id, symptom_id)
 
         for issue in item['issues']:
             issue_id = nodes_id.get(issue['jira_id'], 0)
             if not issue_id:
                 text = issue['factory'] + '\n' + issue['jira_id']
                 issue_id = _create_node(issue['jira_id'], text, 'issue')
-                edges.append(dict(source=symptom_id, target=issue_id, value=1))
+                _create_edge(symptom_id, issue_id)
 
         for cause in item['causes']:
             cause_id = nodes_id.get(cause['cause_id'], 0)
             if not cause_id:
                 cause_id = _create_node(cause['cause_id'], cause['cause_title'], 'cause')
-                edges.append(dict(source=symptom_id, target=cause_id, value=1))
+                _create_edge(symptom_id, cause_id)
 
             for proc in cause.get('procedures', []):
                 text = proc['name'] + '\n' + proc['title']
                 proc_id = _create_node(proc['name'], text, 'procedure')
-                edges.append(dict(source=cause_id, target=proc_id, value=1))
+                _create_edge(cause_id, proc_id)
 
                 for part in proc.get('parts', []):
                     part_id = _create_node(part['id'], part['desc'], 'part')
-                    edges.append(dict(source=proc_id, target=part_id, value=1))
+                    _create_edge(proc_id, part_id)
 
                 for tool in proc.get('tools', []):
                     tool_id = _create_node(tool['id'], tool['desc'], 'tool')
-                    edges.append(dict(source=proc_id, target=tool_id, value=1))
+                    _create_edge(proc_id, tool_id)
+
+    event_codes = [n for n in nodes if n['type'] == 'event_code']
+    if len(event_codes) > 1:  # multiple event codes
+        root_id = _create_node('root', 'root', 'root')
+        for node in event_codes:
+            _create_edge(root_id, node['id'])
+    else:
+        root_id = event_codes[0]['id']
+    nodes[root_id - 1]['span_angle'] = 2 * math.pi
+    calculate_suggested_position(nodes, root_id)
 
     return {"nodes": nodes, "edges": edges}
